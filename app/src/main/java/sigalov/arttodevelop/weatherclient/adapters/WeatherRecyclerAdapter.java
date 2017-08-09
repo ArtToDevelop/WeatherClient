b25440244b1be162a1f81d4bc3d74d9514d7e577
Evgeniy Sigalov <evgeniy.sigalov@tatar.ru>
2017-08-09 11:43:27 +0300

    Отображение направление ветра

--- app/src/main/java/sigalov/arttodevelop/weatherclient/adapters/WeatherRecyclerAdapter.java
+++ app/src/main/java/sigalov/arttodevelop/weatherclient/adapters/WeatherRecyclerAdapter.java
@@ -54,7 +54,7 @@ public class WeatherRecyclerAdapter extends RecyclerView.Adapter<WeatherRecycler
 
         holder.nameTextView.setText(item.getName());
         holder.temperatureTextView.setText(String.format("Температура - %s", item.getTemp()));
-        holder.directionTextView.setText(String.format("Направление ветра - %s", item.getWindDeg()));
+        holder.directionTextView.setText(String.format("Направление ветра - %s", getStringByWindDeg(item.getWindDeg())));
         holder.windTextView.setText(String.format("Скорость ветра - %s", item.getWindSpeed()));
     }
 
@@ -65,4 +65,42 @@ public class WeatherRecyclerAdapter extends RecyclerView.Adapter<WeatherRecycler
 
         return dataList.size();
     }
+
+    private String getStringByWindDeg(Double windDeg)
+    {
+        String degString = windDeg.toString();
+
+        if(windDeg >= 0 && windDeg < 45)
+            return "северный";
+
+        if(windDeg >= 45 && windDeg < 90)
+            return "северо-восточный";
+
+        if(windDeg >= 90 && windDeg < 135)
+            return "восточный";
+
+        if(windDeg >= 135 && windDeg < 180)
+            return "юго-восточный";
+
+        if(windDeg >= 180 && windDeg < 225)
+            return "южный";
+
+        if(windDeg >= 225 && windDeg < 270)
+            return "юго-западный";
+
+        if(windDeg >= 270 && windDeg < 315)
+            return "западный";
+
+        if(windDeg >= 315)
+            return "северо-западный";
+
+        return degString;
+    }
+
+
+//    //Fahrenhiet to Celcius
+//    private double fahrenhietToCelcius(double f)
+//    {
+//        return (f-32)*5/9;
+//    }
 }
